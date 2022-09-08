7ec01d1b9d124fc73080852687ceedfc7f537252
Rennan17 <rsmoura252@gmail.com>
2022-09-08 16:00:35 -0300

    creating model, controller and views

---
+++ pw-01-09/app/Models/PedidoModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\PedidoController;
+
+
+class PedidoModel extends Model
+{
+    protected $table = 'tbpedido';
+}
--- pw-01-09/routes/web.php
+++ pw-01-09/routes/web.php
@@ -2,25 +2,18 @@
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ClienteController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\PedidoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 //Route::get('/contato','ContatoController@index');
 Route::get('contato', [ContatoController::class, 'index']);
 Route::get('categoria', [CategoriaController::class, 'index']);
 Route::get('cliente', [ClienteController::class, 'index']);
+Route::get('produto', [ProdutoController::class, 'index']);
+Route::get('pedido', [PedidoController::class, 'index']);
 //Route::get('/categoria','CategoriaController@index');
 
